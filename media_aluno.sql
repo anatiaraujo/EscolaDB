4c35d85dbbf57b801010b3d7e496ec324199af1d
Natalia Araujo Lima <araujo.natalia27@gmail.com>
2026-05-14 10:32:38 -0300

    Delete media_aluno.sql

--- media_aluno.sql
+++
@@ -1,9 +0,0 @@
-SELECT A.NOME, AVG (N.NOTAFINAL) AS Média 
-FROM Aluno as a
-INNER JOIN Matricula AS M ON M.AlunoId = A.AlunoId
-INNER JOIN NOTA AS N ON N.MATRICULAID = M.MatriculaId
-group by a.Nome
-order by Média desc
-
-
-
