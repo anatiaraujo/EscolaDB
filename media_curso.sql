e063ef2451eb441dcc505151723fd2f3e2652d7f
Natalia Araujo Lima <araujo.natalia27@gmail.com>
2026-05-14 10:32:46 -0300

    Delete media_curso.sql

--- media_curso.sql
+++
@@ -1,9 +0,0 @@
-SELECT C.NOME, AVG (N.NOTAFINAL) AS Média 
-FROM Curso as C
-INNER JOIN Matricula AS M ON M.CursoId = C.CursoId
-INNER JOIN NOTA AS N ON N.MATRICULAID = M.MatriculaId
-group by C.Nome
-order by Média desc
-
-
-
